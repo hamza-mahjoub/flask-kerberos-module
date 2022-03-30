c11cdb318de93a6484adc98e69678555dc782e6a
hamza-mahjoub <60366675+hamza-mahjoub@users.noreply.github.com>
2022-03-30 23:05:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,80 +20,80 @@ def about():
 @requires_authentication
 def home(user):
   concaten = ""
-  liste = []
+  list = []
   for parent, dnames, fnames in os.walk("files/"):
     for fname in fnames:
       filename = os.path.join(parent, fname)
       liste.append(filename)   
-  return render_template('files.html', liste=liste, user=user)
+  return render_template('files.html', list=list, user=user)
 
 
 @app.route('/<path:path>')
 @requires_authentication
 def get_file(user,path,*args, **kwargs):
-  contenu = []
+  content = []
   with open(path) as f:
     for line in f:
-      contenu.append(line)
-  return render_template('content.html', contenu=contenu, user=user) 
+      content.append(line)
+  return render_template('content.html', content=content, user=user) 
 
 @app.route('/addline/<path:path>',methods=['POST'])
 @requires_authentication
 def add_line(user,path,*args, **kwargs):
   data = request.json
   print(data)
-  contenu = []
+  content = []
   f = open(path,"r+")
   for line in f:
-     contenu.append(line)
+     content.append(line)
   for obj in data:
      f.write(obj['message']+'\n')
      contenu.append(obj['message']+'\n')
-  return render_template('content.html', contenu=contenu, user=user) 
+  return render_template('content.html', content=content, user=user) 
  
 @app.route('/deleteline/<path:path>',methods=['POST'])
 @requires_authentication
 def delete_line(user,path,*args, **kwargs):
   data = request.json
-  contenu = []
+  content = []
   with open(path,"r") as f:
     for line in f:
-      contenu.append(line)
+      content.append(line)
    
-  if (data['line'] < 0 or data['line'] >= len(contenu)):
+  if (data['line'] < 0 or data['line'] >= len(content)):
     return Response("precondition failed: can't delete this line !!!", status=412)
  
   with open(path,"w") as f:
       i = 0
-      for line in contenu:
+      for line in content:
         if i != data['line']:
            f.write(line)
         i = i+1
-  contenu = []
+  content = []
   with open(path,"r") as f:
     for line in f:
-      contenu.append(line)
-  return render_template('content.html', contenu=contenu, user=user) 
+      content.append(line)
+  return render_template('content.html', content=content, user=user) 
 
 @app.route('/test/home')
 def test_home():
   user = "hamza"
   concaten = ""
-  liste = []
+  list = []
   for parent, dnames, fnames in os.walk("files/"):
     for fname in fnames:
       filename = os.path.join(parent, fname)
-      liste.append(filename)   
-  return render_template('files.html', liste=liste, user=user)
+      list.append(filename)   
+  return render_template('files.html', list=list, user=user)
   
 @app.route('/test/<path:path>')
 def test_get_file(path,*args, **kwargs):
   user = "hamza"
-  contenu = []
+  content = []
   with open(path) as f:
     for line in f:
-      contenu.append(line)
-  return render_template('content.html', contenu=contenu, user=user) 
+      content.append(line)
+  return render_template('content.html', content=content, user=user) 
 
 if __name__ == '__main__':
 	init_kerberos(app,service='host',hostname='server.example.tn')
